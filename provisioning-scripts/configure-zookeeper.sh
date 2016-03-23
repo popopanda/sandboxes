1046fb6978c95b3d5b2e4e936bc69f7c62bd01a1
Jimmy Y. Huang <jimmy.huang@fundingcircle.com>
2016-03-22 20:10:44 -0700

    work in progress

--- provisioning-scripts/configure-zookeeper.sh
+++ provisioning-scripts/configure-zookeeper.sh
@@ -3,26 +3,26 @@
 # Consul Client Setup
 mv /tmp/report-zookeeper-mode.sh /usr/local/bin/report-zookeeper-mode.sh
 chmod a+x /usr/local/bin/report-zookeeper-mode.sh
+
 mv /tmp/zookeeper-health-check.json /etc/consul.d/zookeeper-health-check.json
 
 mv /tmp/start-consul-client.sh /usr/local/bin/start-consul-client.sh
 chmod a+x /usr/local/bin/start-consul-client.sh
 
 mv /tmp/consul-client.service /etc/systemd/system/consul.service
-mkdir -p /etc/systemd/system/consul.service.d
-mv /tmp/consul-client.service.override /etc/systemd/system/consul.service.d/override.conf
-
-mkdir -p /etc/systemd/system/zookeeper.service.d
-mv /tmp/zookeeper.service.override /etc/systemd/system/zookeeper.service.d/override.conf
-
-mv /tmp/set-zookeeper-id.sh /usr/local/bin/set-zookeeper-id.sh
-chmod a+x /usr/local/bin/set-zookeeper-id.sh
 
 mv /tmp/replace-with-zookeeper-id.sh /usr/local/bin/replace-with-zookeeper-id.sh
 chmod a+x /usr/local/bin/replace-with-zookeeper-id.sh
 
+mkdir -p /etc/systemd/system/consul.service.d
+mv /tmp/consul-client.service.override /etc/systemd/system/consul.service.d/override.conf
+
 # Consul Template Setup
+mv /tmp/bootstrap-zookeeper.sh /usr/local/bin/bootstrap-zookeeper.sh
+chmod a+x /usr/local/bin/bootstrap-zookeeper.sh
+
 mkdir -p /etc/consul-template
+
 mv /tmp/consul-template.cfg /etc/consul-template/consul-template.cfg
 mv /tmp/zoo.cfg.ctmpl /etc/consul-template/zoo.cfg.ctmpl
 
@@ -31,7 +31,14 @@ chmod a+x /usr/local/bin/start-consul-template.sh
 
 mv /tmp/consul-template.service /etc/systemd/system/consul-template.service
 
-mv /tmp/bootstrap-zookeeper.sh /usr/local/bin/bootstrap-zookeeper.sh
-chmod a+x /usr/local/bin/bootstrap-zookeeper.sh
+# Zookeeper Setup
+mv /tmp/set-zookeeper-id.sh /usr/local/bin/set-zookeeper-id.sh
+chmod a+x /usr/local/bin/set-zookeeper-id.sh
+
+mkdir -p /etc/systemd/system/zookeeper.service.d
+mv /tmp/zookeeper.service.override /etc/systemd/system/zookeeper.service.d/override.conf
+
+ZOOKEEPER_DIR=/opt/mesosphere/zookeeper
+echo "pathmunge $ZOOKEEPER_DIR/bin" > /etc/profile.d/zookeeper.sh
 
 systemctl daemon-reload
--- provisioning-scripts/create-hashicorp-directory.sh
+++ provisioning-scripts/create-hashicorp-directory.sh
@@ -3,4 +3,3 @@
 HASHICORP_DIR=/usr/local/bin/hashicorp
 mkdir -p $HASHICORP_DIR
 echo "pathmunge $HASHICORP_DIR" > /etc/profile.d/hashicorp.sh
-chmod a+x /etc/profile.d/hashicorp.sh
